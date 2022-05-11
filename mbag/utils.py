f1bfed0006a7d5d28ad6ae21907bc6fe983c33ab
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-05-11 15:27:11 +0200

    pt3

--- mbag/utils.py
+++ mbag/utils.py
@@ -176,7 +176,7 @@ def data_augmentation_train(mean,std_dev):
         transforms.RandomAdjustSharpness(sharpness_factor=0.20),
         MyGammaCorrection(0.20),
         MyPaddingLongerSide(),
-        transforms.Resize((900,900)),
+        transforms.Resize((800,800)),
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std_dev)
     ])
