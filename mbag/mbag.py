9e00306cd8dcad36706c8ce117021b2ff494873b
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-05-19 16:17:24 +0200

    fix bug2

--- mbag/mbag.py
+++ mbag/mbag.py
@@ -72,13 +72,14 @@ class Bottleneck_18(nn.Module):
         self.bn2 = nn.BatchNorm2d(planes)
         # self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
         # self.bn3 = nn.BatchNorm2d(planes * 4)
+        
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
 
     def forward(self, x, **kwargs):
         residual = x
-
+        
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -87,8 +88,8 @@ class Bottleneck_18(nn.Module):
         out = self.bn2(out)
         out = self.relu(out)
 
-        out = self.conv3(out)
-        out = self.bn3(out)
+        # out = self.conv3(out)
+        # out = self.bn3(out)
 
         if self.downsample is not None:
             residual = self.downsample(x)
