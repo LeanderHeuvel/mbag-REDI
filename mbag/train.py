b1347999c251ab42e7875fac0eb139c5372825a1
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-06-26 14:30:24 +0200

    add flipimage

--- mbag/train.py
+++ mbag/train.py
@@ -412,10 +412,10 @@ if __name__=='__main__':
     dataset_gen_train = utils.BreastCancerDataset_generator(df_train,modality,flipimage, preprocess_train)
     dataloader_train = DataLoader(dataset_gen_train, batch_size=batch_size, shuffle=True, num_workers=num_workers , collate_fn=utils.MyCollate)   
     
-    dataset_gen_val = utils.BreastCancerDataset_generator(df_val,modality,preprocess_val)
+    dataset_gen_val = utils.BreastCancerDataset_generator(df_val,modality,flipimage, preprocess_val)
     dataloader_val = DataLoader(dataset_gen_val, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=utils.MyCollate)
     
-    dataset_gen_test = utils.BreastCancerDataset_generator(df_test,modality,preprocess_val)
+    dataset_gen_test = utils.BreastCancerDataset_generator(df_test,modality,flipimage,preprocess_val)
     dataloader_test = DataLoader(dataset_gen_test, batch_size=batch_size, shuffle=False, num_workers=num_workers, collate_fn=utils.MyCollate)
     
     batches_train=int(math.ceil(train_instances/batch_size))
