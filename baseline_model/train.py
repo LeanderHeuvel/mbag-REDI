b3438149109b234ffbc00aa91152bbab3022df88
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-06-28 14:56:56 +0200

    change plot

--- baseline_model/train.py
+++ baseline_model/train.py
@@ -51,8 +51,8 @@ def results_store_excel(correct_train,total_images_train,train_loss,correct_test
     out.close()
 
 def results_plot(df, file_name, base_path):
-    plt.plot(pd.to_numeric(df['Epoch']),pd.to_numeric(df['Recall Train']),'-r',label='Train Recall')
-    plt.plot(pd.to_numeric(df['Epoch']),pd.to_numeric(df['Recall Val']),'-b',label='Val Recall')
+    plt.plot(pd.to_numeric(df['Epoch']),pd.to_numeric(df['Accuracy Val']),'-r',label='Train Recall')
+    plt.plot(pd.to_numeric(df['Epoch']),pd.to_numeric(df['Accuracy Train']),'-b',label='Val Recall')
     plt.plot(pd.to_numeric(df['Epoch']),pd.to_numeric(df['Avg Loss Train']),'-g',label='Train Loss')
     plt.plot(pd.to_numeric(df['Epoch']),pd.to_numeric(df['Avg Loss Val']),'-y',label='Val Loss')
     plt.legend(loc='upper left')
