babfe276ff4a534120efb1ef63e66e899198c5d4
LeanderHeuvel <leander.vandenheuvel@gmail.com>
2022-07-06 14:18:53 +0200

    fix enum issue

--- mbag/heatmap_plot.py
+++ mbag/heatmap_plot.py
@@ -97,8 +97,10 @@ def analyze_heatmaps(path_to_csv,path_to_data, path_to_figure, image_size=800):
     patch_dict = {0:9,1:17,2:33}
 
     img_names, abnormalities, outcomes = load_csv_data(path_to_csv)
-    for model_idx, groundtruths, model_predictions in enumerate(zip(outcomes[:,0],outcomes[:,1:])):
+    model_idx=0
+    for groundtruths, model_predictions in zip(outcomes[:,0],outcomes[:,1:]):
         patch_size = patch_dict[model_idx]
+        model_idx += 1
         path_to_model = get_model_path_name(patch_size)
         model = load_model(path_to_model, patch_size)
         device = torch.device("cuda")
